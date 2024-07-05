6756a0a2e6c7dce3713a3c045951fe345c88d629
akshithakolan <akshithakolan@gmail.com>
2024-07-05 14:27:18 +0530

    Validating input type and providing proper feedback to the frotend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -17,18 +17,34 @@ class Post(BaseModel):
 
 my_posts = [{"title":"title of post 1","content": "conntent of post 1","id": 1}, {"title": "favourite foods", "content": "I like Pizza", "id": 2}]
 
+def find_posts(id):
+    for p in my_posts:
+        if p ["id"] == id:
+            return p
 
 @app.get("/")
 def root():  
     return {"message": "Hello World"}
 
+#CRUD
+#function to get post
 @app.get("/posts")
 def get_posts():
     return {"data": my_posts}
 
+#function to create post
 @app.post("/posts")
 def create_posts(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0,1000000)
     my_posts.append(post_dict)
     return{"data": post_dict}
+
+#function to get an individual post
+@app.get("/posts/{id}")
+def get_posts(id: int, response: Response):
+
+    post = find_posts(int(id))
+    if not post:
+        response.status_code = 404
+    return{"post_detail": post}
