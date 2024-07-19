a0cf3ea73a47342a13f2e546876e03f44b30f8be
akshithakolan <akshithakolan@gmail.com>
2024-07-19 14:20:47 +0530

    SQLALCHEMY

--- app/schemas.py
+++ app/schemas.py
@@ -12,9 +12,11 @@ class PostCreate(PostBase):
     pass
 
 class Post(BaseModel):
+    id: int
     title: str
     content: str
     published: bool
+    created_at: datetime
 
-class Config:
-    orm_mode = True
+    class Config:
+        orm_mode = True
