de7ac613ab0d95119e978ca803f6245f530b4b1a
akshithakolan <akshithakolan@gmail.com>
2024-07-22 17:34:16 +0530

    Verifying a JWT Token

--- app/oauth2.py
+++ app/oauth2.py
@@ -2,6 +2,10 @@
 from jose import jwt, JWTError
 from datetime import datetime, timedelta
 from . import schemas
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl= 'login')
 
 
 #secret_key, algorithm hs-256, expiration time of the token
@@ -13,22 +17,28 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 def create_access_token(data: dict):
     to_encode = data.copy()
     #provide the time -current to add 30min
-    expire = datetime.now() + timedelta(minutes=(ACCESS_TOKEN_EXPIRE_MINUTES))
+    expire = datetime.utcnow() + timedelta(minutes=(ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
     #to create the jwt token
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-def verify_access_token(token: str, credentials_exeception):
+def verify_access_token(token: str, credentials_exception):
 
     try:
 
 
-        payload = jwt.decode(token,SECRET_KEY, algorithms=ALGORITHM)
-        id: str = payload.get("users_id")
+        payload = jwt.decode(token,SECRET_KEY, algorithms=[ALGORITHM])
+        id: str = payload.get("user_id")
 
         if id is None:
             raise credentials_exeception
         token_data = schemas.TokenData(id=id)
     except JWTError:
-        raise credentials_exeception
+        raise credentials_exception
+    return token_data
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception =  HTTPException(status_code= status.HTTP_401_UNAUTHORIZED, detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+    
+    return verify_access_token(token, credentials_exception)
--- app/routers/post.py
+++ app/routers/post.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
-from .. import models, schemas
+from .. import models, schemas, oauth2
 from ..database  import get_db
 from typing import List
 
@@ -25,12 +25,14 @@ def get_posts(db: Session = Depends(get_db)):
 
 #function to create post
 @router.post("/", status_code = status.HTTP_201_CREATED,response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate,db: Session = Depends(get_db)):
+def create_posts(post: schemas.PostCreate,db: Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
 
     # conn.commit()
     #create a new post
+
+    print(user_id)
     new_post = models.Post(**post.dict())
     #add it to the database
     db.add(new_post) 
